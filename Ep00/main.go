5034e0c05deee22eb06dcca78b033b07ef07579e
StevePro7 <steveprozune@hotmail.com>
2021-04-18 14:23:29 +0100

    player

--- Ep00/main.go
+++ Ep00/main.go
@@ -6,7 +6,54 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+const (
+	screenWidth  = 600
+	screenHeight = 800
+)
+
 func main() {
-	sdl.Init(sdl.INIT_EVERYTHING)
-	fmt.Println("Hello Steve!!")
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		fmt.Println("initializing SDL:", err)
+		return
+	}
+
+	window, err := sdl.CreateWindow(
+		"Gaming in Go Episode 2",
+		sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		screenWidth, screenHeight,
+		sdl.WINDOW_OPENGL)
+	if err != nil {
+		fmt.Println("initializing window:", err)
+		return
+	}
+	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Println("initializing renderer:", err)
+		return
+	}
+	defer renderer.Destroy()
+
+	plr, err := newPlayer(renderer)
+	if err != nil {
+		fmt.Println("creating player:", err)
+		return
+	}
+
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				return
+			}
+		}
+
+		renderer.SetDrawColor(255, 255, 255, 255)
+		renderer.Clear()
+
+		plr.draw(renderer)
+
+		renderer.Present()
+	}
 }
